221dad8c58627daa2313b27146312efbb51da516
赵吉 <zhaoji_vip@163.com>
2022-01-15 12:04:23 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from './store'
 // 导入全局样式
 import './styles/index.less'
 
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+// 注册使用vant组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
